38a498ea86073427e1000000f1c3c29cf62257e7
Thay Huynh <thayvn2007@gmail.com>
2017-09-23 09:50:28 +0700

    Finish anagrams

--- src/main/scala/hackerank/Annagrams.scala
+++ src/main/scala/hackerank/Annagrams.scala
@@ -1,72 +1,42 @@
 package hackerank
 
+import scala.collection.mutable.ListBuffer
+
 object Annagrams {
 
   def main(args: Array[String]): Unit = {
 //    countMinDeletionToMakeAnagramPair(" ccde", "abc12")
 //    countMinDeletionToMakeAnagramPair("a", "aaaaaa")
-//    countMinDeletionToMakeAnagramPair("ab", "aabaaaa")
+    countMinDeletionToMakeAnagramPair("ab", "aabaaaa")
 //    countMinDeletionToMakeAnagramPair("abc", "aabaaaa")
-//    countMinDeletionToMakeAnagramPair("abc", "def")
+    countMinDeletionToMakeAnagramPair("abc", "def")
     countMinDeletionToMakeAnagramPair(
       "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabjkdafklkgflksngdjfidifhoidkfnakfdafadkfdakfkldallknlafsakfjsajkjfklsafalfjalfjkajskfjalfasjkjfklas" +
       "jfnajkfjkasjfsajkfajkjfnajkfjkasjfsajkfajkjfnajkfjkasjfsajkfajkjkdjkhajkdhsajfkjahjfakkioqedncxkkiiqwewouoiasdaasaaafdfdafdfafa" +
         "fdjakfljalkjfiqiqoncklaskanvlalaspoaopaijsfidajfoidahfnandafdafjlkdajfkdajfioiohiadhifiqeohreiqeierohqoirheqirheqiroheqrheqrq",
 
        "jhdjsahjdaacbck")
+
   }
 
-  def countMinDeletionToMakeAnagramPair(first: String, second: String) = {
+  def countMinDeletionToMakeAnagramPair(first: String, second: String): Unit = {
 
-    if(first ==null  || second == null || first.isEmpty || second.isEmpty)
-      throw new IllegalArgumentException("No way to find")
+    if(first ==null  || second == null || first.isEmpty || second.isEmpty) {
+      println(0)
+      return
+    }
 
-    val groupFirstByLetter = first.toStream
-      .map(c => (c,1))
-      .groupBy(p => p._1)
-      .mapValues(_.size)
+    val firstLetters = Array.fill(26){0}
+    val secondLetters = Array.fill(26){0}
+    first.toStream.foreach(c => firstLetters(c - 'a') = firstLetters(c - 'a') + 1)
+    second.toStream.foreach(c => secondLetters(c - 'a') = secondLetters(c - 'a') + 1)
 
-    val groupSecondByLetter = second.toStream
-      .map(c => (c,1))
-      .groupBy(p => p._1)
-      .mapValues(_.size)
+    val result = Array.fill(26){0}
 
-    println(groupFirstByLetter)
-    println(groupSecondByLetter)
+    for(i <- 0 until firstLetters.length) {
+      result(i) = firstLetters(i) - secondLetters(i)
+    }
 
-    val notExistingAtSecond = diff(groupFirstByLetter, groupSecondByLetter)
-    val notExistingAtFirst = diff(groupSecondByLetter, groupFirstByLetter)
-
-    val firstCommonPart = groupFirstByLetter
-      .filterKeys(c => notExistingAtSecond.getOrElse(c, -1) == -1)
-
-    val secondCommonPart = groupSecondByLetter
-      .filterKeys(c => notExistingAtFirst.getOrElse(c, -1) == -1)
-
-    if(firstCommonPart.isEmpty || secondCommonPart.isEmpty)
-      throw new IllegalArgumentException("No way to find")
-
-    val count = firstCommonPart
-      .map(p => Math.abs(secondCommonPart.get(p._1).get - p._2))
-      .reduce(_ + _) + aggregate(notExistingAtSecond) + aggregate(notExistingAtFirst)
-
-    println(count)
+    println(result.reduce(Math.abs(_) + Math.abs(_)))
   }
-
-  def aggregate(letters: Map[Char, Int]) = {
-    letters.aggregate(0)(
-      (b, p) => b + p._2,
-      (b1, b2) => b1 + b2
-    )
-  }
-
-  def diff(letters1: Map[Char,Int], letters2: Map[Char, Int]) = {
-    letters1.filter({
-      case (c: Char, _) => {
-        val count = letters2.getOrElse(c, -1)
-        count == -1
-      }
-    })
-  }
-
 }
